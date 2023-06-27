5d641d370d962d9267f21f7d622959a1fd8ff2f7
aksamitnaja-truna <r.kovalkov04@gmail.com>
2023-06-27 17:25:58 +0300

    Some eddictions

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        byte a, b, c, d;
+        a =  (byte) (number / 1000);
+        b = (byte) ((number / 100) % 10);
+        c = (byte) ((number % 100) / 10);
+        d = (byte) (number % 10);
+        System.out.println(a + b + c + d);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -1,6 +1,13 @@
 package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
+
     public void revert(int number) {
+        byte a, b, c;
+        a =  (byte) (number / 100);
+        b = (byte) ((number / 10) % 10);
+        c = (byte) (number % 10);
+        System.out.println(c * 100 + b * 10 + a);
+
     }
 }
